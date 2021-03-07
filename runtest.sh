45179f1821182d052a4ba0d3267a027bf07d0aac
cyw0ng <dev.cyw0ng@gmail.com>
2021-03-07 16:00:17 +0000

    [framework] add testspace support, contrib to #7
    Signed-off-by: cyw0ng <dev.cyw0ng@gmail.com>

--- runtest.sh
+++ runtest.sh
@@ -1,1 +1,1 @@
-LD_LIBRARY_PATH=`pwd`/service/output/lib:$LD_LIBRARY_PATH ./service/output/bin/ut_test_common
+LD_LIBRARY_PATH=`pwd`/service/output/lib:$LD_LIBRARY_PATH ./service/output/bin/ut_test_common --gtest_output=xml:.test/test_common.xml
